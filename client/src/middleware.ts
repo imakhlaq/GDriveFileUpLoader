1e5eb40f9800783ea058f54e8bfdf2e472cce57e
imakhlaq <imakhlaqXD@gmail.com>
2023-10-09 02:32:22 +0530

    frontend auth

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,3 +1,18 @@
+import { NextResponse } from "next/server";
+import { getAuth } from "@/utils/isAuthenticated";
+
 export function middleware(req: Request) {
   const currentPath = req.url;
+
+  const isAuth = getAuth();
+
+  if (currentPath == "/" && isAuth != null) {
+    NextResponse.redirect(new URL("/login"));
+  }
 }
+
+export const config = {
+  matcher: "/",
+};
+
+//NextResponse.next()==> for go forward
